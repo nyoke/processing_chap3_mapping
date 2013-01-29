7e2fa3583f17688ee516a8941544f92f347a8fc4
Yoshikazu Kawayoke <nyoke.com@gmail.com>
2013-01-29 21:08:49 +0900

    3.1

--- src/Mapping.java
+++ src/Mapping.java
@@ -1,4 +1,16 @@
+import processing.core.*;
 
-public class Mapping {
+public class Mapping extends PApplet {
+	PImage mapImage;
 
+	public void setup() {
+		size(640, 400);
+		mapImage = loadImage("http://benfry.com/writing/map/map.png");
+
+	}
+
+	public void draw() {
+		background(255);
+		image(mapImage, 0, 0);
+	}
 }
